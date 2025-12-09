5edfc9e52a059db663c519f1b434fed8fb24ff01
Jürgen Eckel <juergen@riddleandcode.com>
2025-12-09 15:12:37 +0100

    added DER infra to header and footer

--- scripts/load-header.js
+++ scripts/load-header.js
@@ -81,6 +81,9 @@
         <a class="link-bright w-inline-block" href="product/mypwr.html">
           <p class="no-margin">MYPWR Info</p>
         </a>
+        <a class="link-bright w-inline-block" href="product/infrastructure-for-decentralized-assets.html">
+          <p class="no-margin">DER Infrastructure</p>
+        </a>
         <a class="link-bright w-inline-block" href="product/security-technology.html">
           <p class="no-margin">Security Tech.</p>
         </a>
